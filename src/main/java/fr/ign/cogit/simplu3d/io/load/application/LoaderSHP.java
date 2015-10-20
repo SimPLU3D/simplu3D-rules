abdba1152e51d611ce9f68bc60c9636d42713450
julienperret <julien.perret@gmail.com>
2015-10-20 21:02:33 +0200

    loadNoDTM now uses a File instead of a String.

--- src/main/java/fr/ign/cogit/simplu3d/io/load/application/LoaderSHP.java
+++ src/main/java/fr/ign/cogit/simplu3d/io/load/application/LoaderSHP.java
@@ -1,5 +1,6 @@
 package fr.ign.cogit.simplu3d.io.load.application;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
@@ -28,88 +29,58 @@ import fr.ign.cogit.simplu3d.model.application.Environnement;
  **/
 public class LoaderSHP {
 
-	/*
-	 * Nom des fichiers en entrée
-	 */
-	public static String NOM_FICHIER_ZONAGE = "ZONE_URBA.shp";
-	public static String NOM_FICHIER_PARCELLE = "parcelle.shp";
-	public static String NOM_FICHIER_VOIRIE = "route.shp";
-	public static String NOM_FICHIER_BATIMENTS = "bati.shp";
-	public static String NOM_FICHIER_TERRAIN = "MNT_BD3D.asc";
-	public static String NOM_FICHIER_PRESC_LINEAIRE = "PRESCRIPTION_LIN.shp";
-	public static String NOM_FICHIER_PLU = "DOC_URBA.shp";
+  /*
+   * Nom des fichiers en entrée
+   */
+  public static String NOM_FICHIER_ZONAGE = "ZONE_URBA.shp";
+  public static String NOM_FICHIER_PARCELLE = "parcelle.shp";
+  public static String NOM_FICHIER_VOIRIE = "route.shp";
+  public static String NOM_FICHIER_BATIMENTS = "bati.shp";
+  public static String NOM_FICHIER_TERRAIN = "MNT_BD3D.asc";
+  public static String NOM_FICHIER_PRESC_LINEAIRE = "PRESCRIPTION_LIN.shp";
+  public static String NOM_FICHIER_PLU = "DOC_URBA.shp";
 
-	public Environnement getEnvironnement(String folder) throws Exception {
-		return LoaderSHP.load(folder);
-	}
+  public Environnement getEnvironnement(String folder) throws Exception {
+    return LoaderSHP.load(folder);
+  }
 
-	public static Environnement load(String folder) throws Exception {
+  public static Environnement load(String folder) throws Exception {
+    return load(folder, new FileInputStream(folder + NOM_FICHIER_TERRAIN));
+  }
 
-		return load(folder, new FileInputStream(folder + NOM_FICHIER_TERRAIN));
+  public static Environnement loadNoDTM(File folder) throws Exception {
+    Environnement env = Environnement.getInstance();
+    env.folder = folder.getAbsolutePath();
+    // Chargement des fichiers
+    IFeatureCollection<IFeature> pluColl = ShapefileReader.read(folder + File.separator + NOM_FICHIER_PLU);
+    IFeature featPLU = null;
+    if (!pluColl.isEmpty()) {
+      featPLU = pluColl.get(0);
+    }
+    IFeatureCollection<IFeature> zoneColl = ShapefileReader.read(folder + File.separator + NOM_FICHIER_ZONAGE);
+    IFeatureCollection<IFeature> parcelleColl = ShapefileReader.read(folder + File.separator + NOM_FICHIER_PARCELLE);
+    IFeatureCollection<IFeature> voirieColl = ShapefileReader.read(folder + File.separator + NOM_FICHIER_VOIRIE);
+    IFeatureCollection<IFeature> batiColl = ShapefileReader.read(folder + File.separator + NOM_FICHIER_BATIMENTS);
+    IFeatureCollection<IFeature> prescriptions = ShapefileReader.read(folder + File.separator + NOM_FICHIER_PRESC_LINEAIRE);
+    return LoadFromCollection.load(featPLU, zoneColl, parcelleColl, voirieColl, batiColl, prescriptions, folder.getAbsolutePath(), null);
+  }
 
-	}
-
-	public static Environnement loadNoDTM(String folder) throws Exception {
-		Environnement env = Environnement.getInstance();
-		env.folder = folder;
-
-		// Chargement des fichiers
-		IFeatureCollection<IFeature> pluColl = ShapefileReader.read(folder + NOM_FICHIER_PLU);
-		
-		IFeature featPLU = null;
-		if(! pluColl.isEmpty()){
-			featPLU = pluColl.get(0);
-		}
-		
-		IFeatureCollection<IFeature> zoneColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_ZONAGE);
-		IFeatureCollection<IFeature> parcelleColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_PARCELLE);
-		IFeatureCollection<IFeature> voirieColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_VOIRIE);
-		IFeatureCollection<IFeature> batiColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_BATIMENTS);
-		IFeatureCollection<IFeature> prescriptions = ShapefileReader
-				.read(folder + NOM_FICHIER_PRESC_LINEAIRE);
-
-		return LoadFromCollection.load(featPLU, zoneColl, parcelleColl, voirieColl,
-				batiColl, prescriptions, folder, null);
-	}
-
-	public static Environnement load(String folder, InputStream dtmStream)
-			throws Exception {
-
-		Environnement env = Environnement.getInstance();
-		env.folder = folder;
-
-		// Chargement des fichiers
-
-		// Chargement des fichiers
-		IFeatureCollection<IFeature> pluColl = ShapefileReader.read(folder + NOM_FICHIER_PLU);
-		
-		IFeature featPLU = null;
-		if(! pluColl.isEmpty()){
-			featPLU = pluColl.get(0);
-		}
-		
-		
-		IFeatureCollection<IFeature> zoneColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_ZONAGE);
-		IFeatureCollection<IFeature> parcelleColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_PARCELLE);
-		IFeatureCollection<IFeature> voirieColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_VOIRIE);
-		IFeatureCollection<IFeature> batiColl = ShapefileReader.read(folder
-				+ NOM_FICHIER_BATIMENTS);
-		IFeatureCollection<IFeature> prescriptions = ShapefileReader
-				.read(folder + NOM_FICHIER_PRESC_LINEAIRE);
-
-		// sous-parcelles route sans z, zonage, les bordures etc...
-		DTMArea dtm = new DTMArea(dtmStream, "Terrain", true, 1,
-				ColorShade.BLUE_CYAN_GREEN_YELLOW_WHITE);
-
-		return LoadFromCollection.load(featPLU, zoneColl, parcelleColl, voirieColl,
-				batiColl, prescriptions, folder, dtm);
-	}
-
+  public static Environnement load(String folder, InputStream dtmStream) throws Exception {
+    Environnement env = Environnement.getInstance();
+    env.folder = folder;
+    // Chargement des fichiers
+    IFeatureCollection<IFeature> pluColl = ShapefileReader.read(folder + NOM_FICHIER_PLU);
+    IFeature featPLU = null;
+    if (!pluColl.isEmpty()) {
+      featPLU = pluColl.get(0);
+    }
+    IFeatureCollection<IFeature> zoneColl = ShapefileReader.read(folder + NOM_FICHIER_ZONAGE);
+    IFeatureCollection<IFeature> parcelleColl = ShapefileReader.read(folder + NOM_FICHIER_PARCELLE);
+    IFeatureCollection<IFeature> voirieColl = ShapefileReader.read(folder + NOM_FICHIER_VOIRIE);
+    IFeatureCollection<IFeature> batiColl = ShapefileReader.read(folder + NOM_FICHIER_BATIMENTS);
+    IFeatureCollection<IFeature> prescriptions = ShapefileReader.read(folder + NOM_FICHIER_PRESC_LINEAIRE);
+    // sous-parcelles route sans z, zonage, les bordures etc...
+    DTMArea dtm = new DTMArea(dtmStream, "Terrain", true, 1, ColorShade.BLUE_CYAN_GREEN_YELLOW_WHITE);
+    return LoadFromCollection.load(featPLU, zoneColl, parcelleColl, voirieColl, batiColl, prescriptions, folder, dtm);
+  }
 }
