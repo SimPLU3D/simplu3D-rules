f2abc459a3569994a3cf94f507014ca1dc8c1dd0
Mickael Brasebin <mickael.brasebin@ign.fr>
2017-02-28 14:26:43 +0100

    Renaming and added feature loader for IAUIDFRegulation

--- src/main/java/fr/ign/cogit/simplu3d/checker/experiments/HackUrbaChecker.java
+++ src/main/java/fr/ign/cogit/simplu3d/checker/experiments/HackUrbaChecker.java
@@ -17,92 +17,73 @@ import fr.ign.cogit.simplu3d.checker.model.RuleContext;
 import fr.ign.cogit.simplu3d.checker.model.UnrespectedRule;
 import fr.ign.cogit.simplu3d.model.BasicPropertyUnit;
 import fr.ign.cogit.simplu3d.model.Rules;
-import fr.ign.cogit.simplu3d.model.Rules;
 import fr.ign.cogit.simplu3d.util.SimpleBandProduction;
 
-public class HackUrbaChecker  {
-  
-  
-  
-  private static  CompositeChecker prepareChecker(BasicPropertyUnit bPU,
-      Rules r1, Rules r2, RuleContext context){
-    
-    new SimpleBandProduction(bPU, r1,
-        r2);
+public class HackUrbaChecker {
 
-    CompositeChecker compositeChecker = new CompositeChecker();
+	private static CompositeChecker prepareChecker(BasicPropertyUnit bPU, Rules r1, Rules r2, RuleContext context) {
 
-    
-    DistanceToRoadCheck checkDistanceBand1 = new DistanceToRoadCheck(r1);
-    compositeChecker.addChild(checkDistanceBand1);
-    
-    
-    DistanceToRoadCheck checkDistanceBand2 = new DistanceToRoadCheck(r2);
-    compositeChecker.addChild(checkDistanceBand2);
-    
-    DistanceToBotLimitChecker checkBotLimitB1 = new DistanceToBotLimitChecker(r1);
-    compositeChecker.addChild(checkBotLimitB1);
-    
-    DistanceToBotLimitChecker checkBotLimitB2 = new DistanceToBotLimitChecker(r2);
-    compositeChecker.addChild(checkBotLimitB2);
-    
+		new SimpleBandProduction(bPU, r1, r2);
 
-    CESChecker checkCES = new CESChecker(r1);
-    compositeChecker.addChild(checkCES);
+		CompositeChecker compositeChecker = new CompositeChecker();
 
-    
-    DistanceBetweenBuildingsChecker checkerBuildingBetweenBuildings = new DistanceBetweenBuildingsChecker(r1);
-    compositeChecker.addChild(checkerBuildingBetweenBuildings);
-   
-    
-    DistanceToLatLimitCheck checkDistanceLatB1 = new DistanceToLatLimitCheck(r1);
-    compositeChecker.addChild(checkDistanceLatB1);
+		DistanceToRoadCheck checkDistanceBand1 = new DistanceToRoadCheck(r1);
+		compositeChecker.addChild(checkDistanceBand1);
 
+		DistanceToRoadCheck checkDistanceBand2 = new DistanceToRoadCheck(r2);
+		compositeChecker.addChild(checkDistanceBand2);
 
-    
-    DistanceToLatLimitCheck checkDistanceLatB2 = new DistanceToLatLimitCheck(r2);
-    compositeChecker.addChild(checkDistanceLatB2);
+		DistanceToBotLimitChecker checkBotLimitB1 = new DistanceToBotLimitChecker(r1);
+		compositeChecker.addChild(checkBotLimitB1);
 
+		DistanceToBotLimitChecker checkBotLimitB2 = new DistanceToBotLimitChecker(r2);
+		compositeChecker.addChild(checkBotLimitB2);
 
+		CESChecker checkCES = new CESChecker(r1);
+		compositeChecker.addChild(checkCES);
 
-    ProspectToLatLimitChecker checkPRospectToExistingBuildingB1 = new ProspectToLatLimitChecker(r1);
-    compositeChecker.addChild(checkPRospectToExistingBuildingB1);
-    
-    ProspectToLatLimitChecker checkPRospectToExistingBuildingB2 = new ProspectToLatLimitChecker(r2);
-    compositeChecker.addChild(checkPRospectToExistingBuildingB2);
+		DistanceBetweenBuildingsChecker checkerBuildingBetweenBuildings = new DistanceBetweenBuildingsChecker(r1);
+		compositeChecker.addChild(checkerBuildingBetweenBuildings);
 
+		DistanceToLatLimitCheck checkDistanceLatB1 = new DistanceToLatLimitCheck(r1);
+		compositeChecker.addChild(checkDistanceLatB1);
 
+		DistanceToLatLimitCheck checkDistanceLatB2 = new DistanceToLatLimitCheck(r2);
+		compositeChecker.addChild(checkDistanceLatB2);
 
+		ProspectToLatLimitChecker checkPRospectToExistingBuildingB1 = new ProspectToLatLimitChecker(r1);
+		compositeChecker.addChild(checkPRospectToExistingBuildingB1);
 
-    HeightMaxChecker checkHeightMax = new HeightMaxChecker(r1);
-    compositeChecker.addChild(checkHeightMax);
-    
+		ProspectToLatLimitChecker checkPRospectToExistingBuildingB2 = new ProspectToLatLimitChecker(r2);
+		compositeChecker.addChild(checkPRospectToExistingBuildingB2);
 
-    HeightMaxChecker checkHeightMax2 = new HeightMaxChecker(r2);
-    compositeChecker.addChild(checkHeightMax2);
+		HeightMaxChecker checkHeightMax = new HeightMaxChecker(r1);
+		compositeChecker.addChild(checkHeightMax);
 
+		HeightMaxChecker checkHeightMax2 = new HeightMaxChecker(r2);
+		compositeChecker.addChild(checkHeightMax2);
 
-    ParkingChecker checkParking = new ParkingChecker(r1);
-    compositeChecker.addChild(checkParking);
+		ParkingChecker checkParking = new ParkingChecker(r1);
+		compositeChecker.addChild(checkParking);
 
-    EmptySpaceChecker checkEmpty = new EmptySpaceChecker(r2);
-    compositeChecker.addChild(checkEmpty);
+		EmptySpaceChecker checkEmpty = new EmptySpaceChecker(r2);
+		compositeChecker.addChild(checkEmpty);
 
-    return compositeChecker;
-    
-  }
+		return compositeChecker;
 
-  public static List<UnrespectedRule> check(BasicPropertyUnit bPU,
-      Rules r1, Rules r2, RuleContext context) {
-    
-      return prepareChecker(bPU, r1, r2, context).check(bPU, context);
+	}
 
-  }
-  
-  public static List<GeometricConstraints> generateConstraints(BasicPropertyUnit bPU,
-		  Rules r1, Rules r2, RuleContext context) {
-    
-      return prepareChecker(bPU, r1, r2, context).generate(bPU, context);
+	public static List<UnrespectedRule> check(BasicPropertyUnit bPU, Rules r1, Rules r2, RuleContext context) {
 
-  }
+		return prepareChecker(bPU, r1, r2, context).check(bPU, context);
+
+	}
+
+	public static List<GeometricConstraints> generateConstraints(BasicPropertyUnit bPU, Rules r1, Rules r2,
+			RuleContext context) {
+
+		return prepareChecker(bPU, r1, r2, context).generate(bPU, context);
+
+	}
 }
+
--- src/main/java/fr/ign/cogit/simplu3d/io/regulation/IAUIDFRegulationReader.java
+++ src/main/java/fr/ign/cogit/simplu3d/io/regulation/IAUIDFRegulationReader.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import fr.ign.cogit.geoxygene.api.feature.IFeature;
 import fr.ign.cogit.simplu3d.model.Rules;
 
 public class IAUIDFRegulationReader {
@@ -17,7 +18,91 @@ public class IAUIDFRegulationReader {
 
 	}
 
-	public List<Rules> prepareRegulation(String path) throws IOException {
+	
+	public List<Rules> transformFeatureToRules(IFeature feature){
+
+		System.out.println("*************************************************");
+
+		List<Rules> lRegulation = new ArrayList<>();
+		
+
+		int code_imu = 0;
+		String libelle_zone = feature.getAttribute("Libelle_Zo").toString(); // LIBELLE_ZONE
+		String insee = feature.getAttribute("INSEE").toString();
+		int date_approbation = Integer.parseInt(feature.getAttribute("ANNEE").toString());
+		String libelle_de_base = feature.getAttribute("LIBELLE_DE").toString(); // LIBELLE_DE_BASE
+		String libelle_de_dul = feature.getAttribute("LIBELLE__1").toString(); // LIBELLE_DE_DUL
+		int fonctions = Integer.parseInt(feature.getAttribute("B1_FONCT").toString());
+		int top_zac = Integer.parseInt(feature.getAttribute("B1_TOP_ZAC").toString());
+		int zonage_coherent = Integer.parseInt(feature.getAttribute("B1_ZON_COR").toString());
+		int correction_zonage = Integer.parseInt(feature.getAttribute("B1_ZON_COR").toString());
+		int typ_bande = Integer.parseInt(feature.getAttribute("B1_T_BANDE").toString());
+		int bande = Integer.parseInt(feature.getAttribute("B1_BANDE").toString());
+		Double art_5 = Double.parseDouble(feature.getAttribute("B1_ART_5").toString());
+		Double art_6 = Double.parseDouble(feature.getAttribute("B1_ART_6").toString());
+		int art_71 = Integer.parseInt(feature.getAttribute("B1_ART_71").toString());
+		Double art_72 = Double.parseDouble(feature.getAttribute("B1_ART_72").toString());
+		Double art_73 = Double.parseDouble(feature.getAttribute("B1_ART_73").toString());
+		int art_74 = Integer.parseInt(feature.getAttribute("B1_ART_74").toString());
+		Double art_8 = Double.parseDouble(feature.getAttribute("B1_ART_8").toString());
+		Double art_9 = Double.parseDouble(feature.getAttribute("B1_ART_9").toString());
+		int art_10_top = Integer.parseInt(feature.getAttribute("B1_ART_10T").toString());
+		Double art_101 = Double.parseDouble(feature.getAttribute("B1_ART_10").toString()); // ATTENTION
+																			// A
+																			// CHANGER
+		Double art_102 = Double.parseDouble(feature.getAttribute("B1_HAUT_MT").toString());
+		Double art_12 = Double.parseDouble(feature.getAttribute("B1_ART_12").toString());
+		Double art_13 = Double.parseDouble(feature.getAttribute("B1_ART_13").toString());
+		Double art_14 = Double.parseDouble(feature.getAttribute("B1_ART_14").toString());
+
+		Rules r = new Rules(code_imu, libelle_zone, insee, date_approbation, libelle_de_base,
+				libelle_de_dul, fonctions, top_zac, zonage_coherent, correction_zonage, typ_bande, bande, art_5,
+				art_6, art_71, art_72, art_73, art_74, art_8, art_9, art_10_top, art_101, art_102, art_12, art_13,
+				art_14);
+
+		lRegulation.add(r);
+		r.setNumBand(1);
+
+		System.out.println(r.toString());
+
+		if (bande != 0) {
+			int fonctions_2 = Integer.parseInt(feature.getAttribute("B2_FONCT").toString());
+			Double art_5_2 = Double.parseDouble(feature.getAttribute("B2_ART_5").toString());
+			Double art_6_2 = Double.parseDouble(feature.getAttribute("B2_ART_6").toString());
+			int art_71_2 = Integer.parseInt(feature.getAttribute("B2_ART_71").toString());
+			Double art_72_2 = Double.parseDouble(feature.getAttribute("B2_ART_72").toString());
+			Double art_73_2 = Double.parseDouble(feature.getAttribute("B2_ART_73").toString());
+			int art_74_2 = Integer.parseInt(feature.getAttribute("B2_ART_74").toString());
+			Double art_8_2 = Double.parseDouble(feature.getAttribute("B2_ART_8").toString());
+			Double art_9_2 = Double.parseDouble(feature.getAttribute("B2_ART_9").toString());
+			int art_10_top_2 = Integer.parseInt(feature.getAttribute("B2_ART_10T").toString());
+			Double art_101_2 = Double.parseDouble(feature.getAttribute("B2_ART_10").toString()); // ATTENTION
+																					// A
+																					// CHANGER
+			Double art_102_2 = Double.parseDouble(feature.getAttribute("B2_HAUT_M").toString());
+			Double art_12_2 = Double.parseDouble(feature.getAttribute("B2_ART_12").toString());
+			Double art_13_2 = Double.parseDouble(feature.getAttribute("B2_ART_13").toString());
+			Double art_14_2 = Double.parseDouble(feature.getAttribute("B2_ART_14").toString());
+			
+
+			Rules r2 = new Rules(code_imu, libelle_zone, insee, date_approbation, libelle_de_base,
+					libelle_de_dul, fonctions_2, top_zac, zonage_coherent, correction_zonage, typ_bande, bande,
+					art_5_2, art_6_2, art_71_2, art_72_2, art_73_2, art_74_2, art_8_2, art_9_2, art_10_top_2,
+					art_101_2, art_102_2, art_12_2, art_13_2, art_14_2);
+
+			System.out.println(r2.toString());
+
+			lRegulation.add(r2);
+			r2.setNumBand(2);
+		}
+
+		System.out.println("*************************************************");
+		return lRegulation;
+	}
+	
+	
+	
+	public List<Rules> readRegulationFromCSV(String path) throws IOException {
 
 		File f = new File(path);
 
@@ -68,7 +153,7 @@ public class IAUIDFRegulationReader {
 			Double art_101 = Double.parseDouble(newmap.get("B1_ART_10").toString()); // ATTENTION
 																				// A
 																				// CHANGER
-			Double art_102 =Double.parseDouble(newmap.get("B1_HAUT_MT").toString());
+			Double art_102 = Double.parseDouble(newmap.get("B1_HAUT_MT").toString());
 			Double art_12 = Double.parseDouble(newmap.get("B1_ART_12").toString());
 			Double art_13 = Double.parseDouble(newmap.get("B1_ART_13").toString());
 			Double art_14 = Double.parseDouble(newmap.get("B1_ART_14").toString());
@@ -125,3 +210,4 @@ public class IAUIDFRegulationReader {
 	}
 
 }
+
