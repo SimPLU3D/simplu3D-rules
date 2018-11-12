4213de940139edfb4ee771a3a1ad6341818ac7a5
Mickaël Brasebin <mickael.brasebin@ign.fr>
2018-11-12 12:29:31 +0100

    Adding regulation diagram

--- src/main/java/fr/ign/cogit/simplu3d/model/IZoneRegulation.java
+++ src/main/java/fr/ign/cogit/simplu3d/model/IZoneRegulation.java
@@ -1,9 +1,27 @@
 package fr.ign.cogit.simplu3d.model;
 
+/**
+ * Interface permettant de définir les contraintes réglementaires et les
+ * paramètres de règles s'appliquant au niveau d'une zone réglementaire.
+ * 
+ * 
+ * @author mbrasebin
+ *
+ */
 public interface IZoneRegulation {
-	
+
+	/**
+	 * 
+	 * @return La zone urbaine sur laquelle s'applique les contraintes
+	 *         réglementaires définies dans l'interface
+	 */
 	public UrbaZone getUrbaZone();
-	
+
+	/**
+	 * 
+	 * @return Une représentation textuelle pour mieux comprendre le contenu de la
+	 *         classe implémentée.
+	 */
 	public String toText();
 
 }
